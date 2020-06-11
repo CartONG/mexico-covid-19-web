3d2d496f431c0b83c66cf4fdb5889b21a4c74a34
jmart <mail@jmartin.tf>
2020-06-11 18:49:29 +0200

    fix autocomplete

--- src/primary/dropdown/Dropdown.component.ts
+++ src/primary/dropdown/Dropdown.component.ts
@@ -65,7 +65,6 @@ export default class Dropdown extends Vue {
 
   selectItem(item: StateSummary | MunicipalitySummary | SchoolSummary | null) {
     if (item === null) {
-      this.appStore().select(null);
       return;
     }
 
