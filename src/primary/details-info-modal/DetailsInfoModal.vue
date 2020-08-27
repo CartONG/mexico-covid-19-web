9f70bbcfc54ad6430300bc34ba91f9bfdb6ff9b4
jmart <mail@jmartin.tf>
2020-08-27 09:12:34 +0200

    update info box content

--- src/primary/details-info-modal/DetailsInfoModal.vue
+++ src/primary/details-info-modal/DetailsInfoModal.vue
@@ -7,15 +7,13 @@
     </a>
     <b-modal :active="active" @close="active = false">
       <article class="message px-2 has-no-background">
-        <div class="message-header has-background-secondary">
-          <p>Lorem ipsum</p>
-        </div>
         <div class="message-body has-text-justified has-background-white content">
           <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum."
+            Haga click con el cursor sobre la flecha marrón (
+            <span class="icon has-text-primary-bis"><i class="mdi mdi-24px mdi-chevron-up"></i></span>
+            ) para obtener más información y estadísticas. Haga click de nuevo sobre la flecha (
+            <span class="icon has-text-primary-bis"><i class="mdi mdi-24px mdi-chevron-down"></i></span>
+            ) para ocultar la tarjeta de información.
           </p>
         </div>
       </article>
